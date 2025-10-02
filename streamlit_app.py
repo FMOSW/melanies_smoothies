c9bb3f64feaa5ffc0f7776bb295d3f7292bcacce
FMOSW <faith.moore@ometis.co.uk>
2025-10-02 15:17:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,5 +62,5 @@ if ingredients_list:
         st.success(message)
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+# smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
