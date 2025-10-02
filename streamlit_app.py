9400f49190f3adba8b7bcb8142c950e58b4dabec
FMOSW <faith.moore@ometis.co.uk>
2025-10-02 15:23:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
+# st.dataframe(pd_df)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -52,7 +52,7 @@ if ingredients_list:
     my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
-    st.write(my_insert_stmt)
+    # st.write(my_insert_stmt)
     
     time_to_insert = st.button('Submit Order')
 
@@ -61,6 +61,5 @@ if ingredients_list:
         message = f'Your smoothie is ordered, {name_on_order}! ✅'
         st.success(message)
 
-
-# smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
